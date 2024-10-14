7f14909af87918ebe0e36fc5b4935071fd1232ac
abdul-ruet <abdul.ruet@gmail.com>
2024-10-15 00:21:37 +0600

    final submit

--- script.js
+++ script.js
@@ -4,5 +4,18 @@ function showJessicaInfo() {
     document.querySelector(".chat-history").style.display = "none";
   }
   
+  var myHeaders = new Headers();
+myHeaders.append("Authorization", "Basic [coalition, skill-test]");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch("https://fedskillstest.coalitiontechnologies.workers.dev", requestOptions)
+  .then(response => response.json())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
   
-  
+ 
