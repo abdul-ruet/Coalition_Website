c83e903e3922ce57e3807568f39ade35d1bba4a2
abdul-ruet <abdul.ruet@gmail.com>
2024-10-14 21:45:49 +0600

    project completed

---
+++ script.js
@@ -0,0 +1,8 @@
+// Hide Jessica's info , showing it when clicked
+function showJessicaInfo() {
+    document.getElementById("jessica-info").style.display = "block";
+    document.querySelector(".chat-history").style.display = "none";
+  }
+  
+  
+  
